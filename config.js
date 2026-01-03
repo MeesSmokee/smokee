422d6366a11254482405015e40ff460d8fcde4c1
MeesSmokee <the415994@gmail.com>
2026-01-03 11:59:55 +0300

    Update and rename config.js.txt to config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ window.SITE_CONFIG = {
   // حط رابط اليوتيوب كامل (مو ID)
   youtubeUrl: "Zkm2_E1mfZQ",
 
-  // إذا عندك أغنية ملف داخل الريبو: "song.mp3"
+  // إذا عندك أغنية ملف داخل الريبو: "theme.mp3"
   // إذا رابط مباشر خارجي: "https://f.top4top.io/m_3655yw33w1.mp3"
-  musicUrl: "song.mp3",
+  musicUrl: "theme.mp3",
 
   // يبدأ يوتيوب مكتوم
   startYoutubeMuted: true,
@@ -16,3 +16,4 @@ window.SITE_CONFIG = {
   youtubeVolume: 70,
   musicVolume: 0.75
 };
+
